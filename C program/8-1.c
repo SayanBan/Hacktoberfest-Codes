8e4c35840319feb73aed4e0e807b2df8fc72662c
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-09-30 21:33:15 +0530

    Add files via upload

---
+++ C program/8-1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int length(char *s)
+{
+	int i=0;
+	while(*(s+i)!='\0')
+	{
+		i++;
+	}
+	return i;	
+}
+void main()
+{
+	char s[100];
+	int len;
+	puts("Enter the string ");
+	gets(s);
+	len=length(s);
+	printf("Length of String is : %d",len);
+}
