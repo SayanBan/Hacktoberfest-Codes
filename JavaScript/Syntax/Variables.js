30ba604a60a03572d783a2944b4af5071b90e037
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:36:45 +0530

    Create Variables.js

---
+++ JavaScript/Syntax/Variables.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p>In this example, x is defined as a variable.
+Then, x is assigned the value of 6:</p>
+
+<p id="demo"></p>
+
+<script>
+var x;
+x = 6;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
