259aceadf68b4c87d318090fe88299341adebbd6
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:39:46 +0530

    Create Keywords.js

---
+++ JavaScript/Syntax/Keywords.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>The var Keyword Creates Variables</h2>
+
+<p id="demo"></p>
+
+<script>
+var x, y;
+x = 5 + 6;
+y = x * 10;
+document.getElementById("demo").innerHTML = y;
+</script>
+
+</body>
+</html>
