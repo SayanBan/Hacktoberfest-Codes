1e91178d87e1a7b397cd8e6c4e614c897b168989
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:35:29 +0530

    Create Statements.js

---
+++ JavaScript/Syntax/Statements.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Statements</h2>
+
+<p>A <b>JavaScript program</b> is a list of <b>statements</b> to be executed by a computer.</p>
+
+<p id="demo"></p>
+
+<script>
+var x, y, z;  // Declare 3 variables
+x = 5;    // Assign the value 5 to x
+y = 6;    // Assign the value 6 to y
+z = x + y;  // Assign the sum of x and y to z
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";
+</script>
+
+</body>
+</html>
