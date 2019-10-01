10f0d98067f1425fece5a37a6d01914e60919514
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:25:50 +0530

    Create Undefined Variable.js

---
+++ JavaScript/Declarations/Undefined Variable.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript</h2>
+
+<p>The value (and the data type) of a variable with no value is <b>undefined</b>.</p>
+
+<p id="demo"></p>
+
+<script>
+var car;
+document.getElementById("demo").innerHTML =
+car + "<br>" + typeof car;
+</script>
+
+</body>
+</html> 
