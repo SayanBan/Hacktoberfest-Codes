36f741610cb8fd89161c04ab080c41c295e58d3c
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:32:23 +0530

    Create Function Call without ().js

---
+++ JavaScript/Functions/Function Call without ().js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Functions</h2>
+
+<p>Accessing a function without () will return the function definition instead of the function result:</p>
+<p id="demo"></p>
+
+<script>
+function toCelsius(f) {
+  return (5/9) * (f-32);
+}
+document.getElementById("demo").innerHTML = toCelsius;
+</script>
+
+</body>
+</html>
