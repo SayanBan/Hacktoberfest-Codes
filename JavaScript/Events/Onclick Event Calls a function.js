e5040b2baa4d93a21ff3b644e9535afa8debb6a7
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:34:42 +0530

    Create Onclick Event Calls a function.js

---
+++ JavaScript/Events/Onclick Event Calls a function.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to display the date.</p>
+
+<button onclick="displayDate()">The time is?</button>
+
+<script>
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
+</script>
+
+<p id="demo"></p>
+
+</body>
+</html> 
