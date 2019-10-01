a55ecd0be27ccea5c30c694426bb978a82609488
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:37:11 +0530

    Create Operators.js

---
+++ JavaScript/Syntax/Operators.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Operators</h2>
+
+<p>JavaScript uses arithmetic operators to compute values (just like algebra).</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = (5 + 6) * 10;
+</script>
+
+</body>
+</html>
