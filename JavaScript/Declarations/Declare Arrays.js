33f0e082e6a1ee83aa2787daeb65b449155f856b
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:22:58 +0530

    Create Declare Arrays.js

---
+++ JavaScript/Declarations/Declare Arrays.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Arrays</h2>
+
+<p>Array indexes are zero-based, which means the first item is [0].</p>
+
+<p id="demo"></p>
+
+<script>
+var cars = ["Saab","Volvo","BMW"];
+
+document.getElementById("demo").innerHTML = cars[0];
+</script>
+
+</body>
+</html>
