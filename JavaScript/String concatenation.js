af757c17f79bdfa50c18c9d253b69ae670d5574b
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-10-01 11:19:15 +0530

    Create String concatenation.js

---
+++ JavaScript/String concatenation.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Operators</h2>
+
+<p>The + operator concatenates (adds) strings.</p>
+
+<p id="demo"></p>
+
+<script>
+var txt1 = "What a very";
+var txt2 = "nice day";
+document.getElementById("demo").innerHTML = txt1 + txt2;
+</script>
+
+</body>
+</html>
