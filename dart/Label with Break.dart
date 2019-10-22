f82005a28fb347de31ee6197b805780f1a39c124
AvikMukherjee007 <56878915+AvikMukherjee007@users.noreply.github.com>
2019-10-22 19:07:16 +0530

    Create Label with Break.dart

---
+++ dart/Label with Break.dart
@@ -0,0 +1,23 @@
+Live Demo
+
+void main() { 
+   outerloop: // This is the label name 
+   
+   for (var i = 0; i < 5; i++) { 
+      print("Innerloop: ${i}"); 
+      innerloop: 
+      
+      for (var j = 0; j < 5; j++) { 
+         if (j > 3 ) break ; 
+         
+         // Quit the innermost loop 
+         if (i == 2) break innerloop; 
+         
+         // Do the same thing 
+         if (i == 4) break outerloop; 
+         
+         // Quit the outer loop 
+         print("Innerloop: ${j}"); 
+      } 
+   } 
+}
